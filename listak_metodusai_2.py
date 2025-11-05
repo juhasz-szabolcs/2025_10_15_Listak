de4e095f2a21af6cab0492adb845b2250d5faca2
Juhász Szabolcs <juhaszszabolcs@pg.local>
2025-11-05 07:34:36 +0100

    s

---
+++ listak_metodusai_2.py
@@ -0,0 +1,23 @@
+honapok = ['január', 'február','március', 'aprilis', 'május', 'június']
+# honapok = [1, 3, 5, 2]
+
+# sorbarendezi a listát
+sorted_honapok = sorted(honapok)
+print(honapok)
+print(sorted_honapok)
+# print(honapok.sort())
+# print(honapok)
+
+reversed_honapok = sorted(honapok, reverse=True)
+print(reversed_honapok)
+
+# az adott elem első előfordulásának indexe
+print(honapok.index('március'))
+
+print("április" in honapok)
+
+
+
+# 1.1
+# 1.2
+# 2.
